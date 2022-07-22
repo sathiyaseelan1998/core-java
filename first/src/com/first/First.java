839e5ddb232a6e1a7437dbb5ecbdbd2962a63c6c
sathiyaseelan1998 <sathiyaseelan2598@gmail.com>
2022-07-22 17:31:02 +0530

    added to for loop

--- first/src/com/first/First.java
+++ first/src/com/first/First.java
@@ -5,6 +5,12 @@ public class First {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("welcome to all");
+		
+		for(int i=1;i<=10;i++)
+		{
+			System.out.println(1);
+
+		}
 
 	}
 
