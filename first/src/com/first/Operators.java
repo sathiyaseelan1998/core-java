d95042a97d1fa8e28ffbaf2b2e64818c54e03bf7
sathiyaseelan1998 <sathiyaseelan2598@gmail.com>
2022-07-23 13:33:08 +0530

    change

--- first/src/com/first/Operators.java
+++ first/src/com/first/Operators.java
@@ -5,8 +5,8 @@ public class Operators {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int m1=98;
-		int m2=89;
+		int m1=99;
+		int m2=100;
 		System.out.println("======ARITHMATIC OPERATION======");
 		System.out.println(m1+m2);
 		System.out.println(m1-m2);
