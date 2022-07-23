3fcf46ae1bc2ad5b15cac6e07457781187c59e0c
sathiyaseelan1998 <sathiyaseelan2598@gmail.com>
2022-07-23 09:31:14 +0530

    arithmetic operation

---
+++ first/src/com/first/Operators.java
@@ -0,0 +1,19 @@
+package com.first;
+
+public class Operators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int m1=80;
+		int m2=70;
+		System.out.println("======ARITHMATIC OPERATION======");
+		System.out.println(m1+m2);
+		System.out.println(m1-m2);
+		System.out.println(m1*m2);
+		System.out.println(m1/m2);
+		System.out.println(m1%m2);
+
+	}
+
+}
