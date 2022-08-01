430fd73ae71b9a9f8733647777ae48e2374ae658
sathiyaseelan1998 <sathiyaseelan2598@gmail.com>
2022-08-01 09:22:42 +0530

    if-else

--- first/src/com/first/Condition.java
+++ first/src/com/first/Condition.java
@@ -4,6 +4,16 @@ public class Condition {
 
 	public static void main(String[] args) {
 		
+		int mark=23;
+		
+		if(mark>=35)
+		{
+			System.out.println("pass");
+		}
+		else
+		{
+			System.out.println("fail");
+		}
 
 	}
 }
