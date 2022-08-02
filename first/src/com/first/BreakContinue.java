84a579482f2fba1dd119adc3c6ada783eb658552
sathiyaseelan1998 <sathiyaseelan2598@gmail.com>
2022-08-02 08:34:51 +0530

    break and continue

---
+++ first/src/com/first/BreakContinue.java
@@ -0,0 +1,32 @@
+package com.first;
+
+public class BreakContinue {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("=====Break=====");
+
+
+		for(int i=0;i<=5;i++){
+			
+			if(i==3){
+				break;
+			}
+			System.out.println(i);
+		}
+		
+		System.out.println("=====Continue=====");
+
+
+		for(int i=0;i<=5;i++){
+			
+			if(i==3){
+				continue;
+			}
+			System.out.println(i);
+		}
+		
+	}
+
+}
