8623ee19c98e63449161e6461884df558ab9fe86
sathiyaseelan1998 <sathiyaseelan2598@gmail.com>
2022-08-02 08:50:51 +0530

    datatypes

---
+++ first/src/com/first/DataTypes.java
@@ -0,0 +1,22 @@
+package com.first;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char a='a';
+		short b=10;
+		byte c=100;
+		int d=1000;
+		long e=10000;
+		boolean f=true;
+		float g=10.5f;
+		double h=100.777d;
+		String i="java";
+		
+		System.out.println(a+"--" +b+"--" +c+"--" +d+"--" +e+"--" +f+"--" +g+"--" +h+"--" +i);
+
+	}
+
+}
