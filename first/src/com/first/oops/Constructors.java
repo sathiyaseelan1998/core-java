83b5a2dc3a71b80ef44a72f6075faeff05c6ca93
sathiyaseelan1998 <sathiyaseelan2598@gmail.com>
2022-08-03 09:57:39 +0530

    some method added

--- first/src/com/first/oops/Constructors.java
+++ first/src/com/first/oops/Constructors.java
@@ -10,10 +10,16 @@ public class Constructors  {
 		System.out.println("parameterized constructors");
 		System.out.println(name+"---"+age);
 	}
+	void get()
+	{
+		System.out.println("method calling...");
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Constructors con=new Constructors();
 		Constructors cons=new Constructors("dhoni",38);
+		con.get();
+		cons.get();
 	}
 
 }
