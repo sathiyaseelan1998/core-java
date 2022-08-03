fcf3c9bcb1d8c319a70751b4a614b6c06f8d09b3
sathiyaseelan1998 <sathiyaseelan2598@gmail.com>
2022-08-03 09:50:42 +0530

    parameterized constructors

--- first/src/com/first/oops/Constructors.java
+++ first/src/com/first/oops/Constructors.java
@@ -6,11 +6,14 @@ public class Constructors  {
 	{
 		System.out.println("default constructors");
 	}
-	
+	Constructors(String name,int age){
+		System.out.println("parameterized constructors");
+		System.out.println(name+"---"+age);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Constructors con=new Constructors();
-
+		Constructors cons=new Constructors("dhoni",38);
 	}
 
 }
