3e51253d637be129c047e82dd3e719a6650ed46e
sathiyaseelan1998 <sathiyaseelan2598@gmail.com>
2022-08-05 17:01:44 +0530

    using float

--- first/src/com/first/basic/Returns.java
+++ first/src/com/first/basic/Returns.java
@@ -11,11 +11,16 @@ public class Returns {
 		return name;
 		
 	}
+	public float division(float a,float b) {
+		return a/b;
+		
+	}
 	
 public static void main(String arg[]) {
 	Returns r=new Returns();
 	System.out.println(r.add(100,400));
-	System.out.println(r.info("rohit"));	
+	System.out.println(r.info("rohit"));
+	System.out.println(r.division(50,35));
 	}
 
 }
