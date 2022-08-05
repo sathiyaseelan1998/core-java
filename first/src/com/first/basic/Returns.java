469dc77ee19a40671182c5f2b7fe76b9e0c3f013
sathiyaseelan1998 <sathiyaseelan2598@gmail.com>
2022-08-05 16:55:52 +0530

    return keyword

---
+++ first/src/com/first/basic/Returns.java
@@ -0,0 +1,17 @@
+package com.first.basic;
+
+
+public class Returns {
+	
+	public int add(int a,int b) {
+		return a+b;
+		
+	}
+	
+public static void main(String arg[]) {
+	Returns r=new Returns();
+	System.out.println(r.add(100,400));
+		
+	}
+
+}
