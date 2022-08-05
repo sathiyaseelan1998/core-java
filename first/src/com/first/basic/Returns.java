2ac488162e0d054a5ed79024f2c8119e75ba8e6e
sathiyaseelan1998 <sathiyaseelan2598@gmail.com>
2022-08-05 16:59:06 +0530

    return statement using by string

--- first/src/com/first/basic/Returns.java
+++ first/src/com/first/basic/Returns.java
@@ -7,11 +7,15 @@ public class Returns {
 		return a+b;
 		
 	}
+	public String info(String name) {
+		return name;
+		
+	}
 	
 public static void main(String arg[]) {
 	Returns r=new Returns();
 	System.out.println(r.add(100,400));
-		
+	System.out.println(r.info("rohit"));	
 	}
 
 }
