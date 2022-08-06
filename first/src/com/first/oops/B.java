615e3d7a4b85353ad45fe324f6d73f561c8898e2
sathiyaseelan1998 <sathiyaseelan2598@gmail.com>
2022-08-06 14:53:03 +0530

    multiple inheritance

---
+++ first/src/com/first/oops/B.java
@@ -0,0 +1,9 @@
+package com.first.oops;
+
+public class B extends A{
+
+	void b() {
+		System.out.println("sub class of class A");
+	}
+	
+}
--- first/src/com/first/oops/Inheritance.java
+++ first/src/com/first/oops/Inheritance.java
@@ -1,11 +1,12 @@
 package com.first.oops;
 
-public class Inheritance extends A {
+public class Inheritance extends B {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Inheritance in=new Inheritance();
 		in.a();
+		in.b();
 	}
 
 }
