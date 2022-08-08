817c0c2a3b9f51b4dd818b59471ddab48b32470c
sathiyaseelan1998 <sathiyaseelan2598@gmail.com>
2022-08-08 08:29:50 +0530

    hierarchical

---
+++ first/src/com/first/oops/C.java
@@ -0,0 +1,15 @@
+package com.first.oops;
+
+public class C extends A {
+	void c() {
+		System.out.println("sub class of A");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		C c=new C();
+		c.c();
+		c.a();
+		
+	}
+
+}
